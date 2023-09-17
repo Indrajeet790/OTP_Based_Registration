5137a7d8c58873bf4823ceb36c4c7a04648b9cd0
indrajeet790 <indrajeetkumarnayak3@gmail.com>
2023-09-17 13:56:56 +0530

    add databse to env

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 // connect to the database
-mongoose.connect("mongodb+srv://Otp_based_Registration:Gupta&&1998@cluster0.oidggmh.mongodb.net/");
+mongoose.connect(process.env.MONGODB_URI);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "error connecting to db"));
