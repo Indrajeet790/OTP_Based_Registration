fd306abc142b6af8552697b89c6132eff4db0afa
indrajeet790 <indrajeetkumarnayak3@gmail.com>
2023-09-20 13:32:12 +0530

    my main file code

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-require("dotenv/config")
-const express=require("express");
-const app=express();
-const db=require("./config/dbConnection")
+const dotEnv = require("dotenv/config");
+const express = require("express");
+const app = express();
+const db = require("./config/dbConnection")
+const bodyParser = require("body-parser");
 
-const port=process.env.PORT;
+const port = process.env.PORT ;
 
-app.set("view engine", "ejs");
-app.set("views", "./views");
-app.use(express.static(__dirname + "/assets"));
 
-app.use("/",require("./routes"))
+app.use(bodyParser.json());
+app.use("/", require("./routes"));
 
-app.listen(port,(err)=>{
+app.listen(port, (err) => {
     if(err){
-        console.log("server is not running");
+        console.log({Error: "Failed To Load Server ", err});
     }else{
-        console.log(`server is running on ${port}`);
-        
+        console.log(`Server is running on port ${port} `);
     }
 })
